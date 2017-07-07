de5ba42c435f8a99cbc53501db6ad67d8d7be69d
julien <auber23gine>
2017-07-07 21:41:04 +1000

    Bug fix

--- join2_reducer.py
+++ join2_reducer.py
@@ -4,15 +4,24 @@ import sys
 
 tv_show = ""
 count = 0
+is_abc = False
+
 for line in sys.stdin:
     key, value = line.split("\t")
     key = key.strip()
     value = value.strip()
 
+    if key != tv_show:
+        if is_abc:
+            print format("%s\t%s" % (tv_show, count))
+        is_abc = False
+        count = 0
+        tv_show = key
+
     if value == "ABC":
-        print format("%s\t%s" % (tv_show, count))
+        is_abc = True
     else:
-        if key != tv_show:
-            count = 0
-            tv_show = key
         count += int(value)
+
+if is_abc:
+    print format("%s\t%s" % (tv_show, count))
